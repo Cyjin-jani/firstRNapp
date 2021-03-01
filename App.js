f3469da18d4cf7de4a655c6ceb9499f1b5264f7e
yjcho <jinjanic91@gmail.com>
2021-03-01 22:40:17 +0900

    final commit

--- App.js
+++ App.js
@@ -21,9 +21,9 @@ export default class extends React.Component {
   }
 
   getWeather = async (latitude, longitude) => {
-    const { data } = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`);
+    const { data: {main: {temp}, weather} } = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`);
     // console.log(data.main.temp);
-    this.setState({isLoading: false, temp: data.main.temp})
+    this.setState({isLoading: false, temp, condition: weather[0].main})
   };
 
   getLocation = async () => {
@@ -46,7 +46,7 @@ export default class extends React.Component {
   }
 
   render() {
-    const { isLoading, temp } = this.state;
-    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} />;
+    const { isLoading, temp, condition } = this.state;
+    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} condition={condition} />;
   }
 }
